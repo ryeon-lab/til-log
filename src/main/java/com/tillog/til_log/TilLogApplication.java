9b8bf7f80d1adfa55c14a2a861935dd15a816998
ryeon-xx <rlarkgus3415@gmail.com>
2026-09-19 01:24:20 +0900

    Initial commit: Spring Boot + MySQL 프로젝트 세팅

---
+++ src/main/java/com/tillog/til_log/TilLogApplication.java
@@ -0,0 +1,13 @@
+package com.tillog.til_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tillog/til_log/TilLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tillog.til_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
